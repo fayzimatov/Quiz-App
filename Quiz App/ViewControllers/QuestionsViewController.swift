f247a4d83fa02106d4e4f3d757cad08fb21c770c
UmidKhan <ufayzimatov4@gmail.com>
2024-11-28 14:51:47 +0500

    Removed storyboard, added new files

--- Quiz App/ViewControllers/QuestionsViewController.swift
+++ Quiz App/ViewControllers/QuestionsViewController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class QuestionsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+
     }
 
 
